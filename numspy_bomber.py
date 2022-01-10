9d7421be666bcc5ca3a3125a93d77eae1a1713dc
ponshnam <97454550+ponshnam@users.noreply.github.com>
2022-01-10 16:12:51 +0200

    Update numspy_bomber.py
    ваы

--- numspy_bomber.py
+++ numspy_bomber.py
@@ -3,12 +3,12 @@ from numspy import Way2sms
 try:
 	w2s = Way2sms()
 
-	mobile_number= int(input("Enter the 10 digit Mobile number on which you want bomb: "))
-	message= input("Enter message: ")
-	n = int(input("How many messsages you want to send? (limit is 100/day): "))
+	mobile_number= int(input("+380684428743: "))
+	message= input("Коля бот: ")
+	n = int(input("10 (limit is 1000/day): "))
 
-	username = input("Your way2sms username: ")
-	password = input("Your way2sms password: ")
+	username = input("Петя: ")
+	password = input("Katess: ")
 	w2s.login(username,password)
 
 	for i in range(0,n):
